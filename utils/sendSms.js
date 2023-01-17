4ea00807af5af6c3a1050442e8784d51062186e5
Rahul Pradhan <pradhanrahul3346@gmail.com>
2023-01-17 14:50:35 +0530

    done

--- utils/sendSms.js
+++ utils/sendSms.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const sendSMS = async (number, code, otp) => {
   try {
-    const sms = await axios.get(
-      `https://api.authkey.io/request?authkey=${process.env.AUTH_KEY}&mobile=${number}&country_code=${code}&sid=6737&name=UPONE&otp=${otp}`
-    );
+    // const sms = await axios.get(
+    //   `https://api.authkey.io/request?authkey=${process.env.AUTH_KEY}&mobile=${number}&country_code=${code}&sid=6737&name=UPONE&otp=${otp}`
+    // );
     console.log("Success");
   } catch (error) {
     console.log(error);

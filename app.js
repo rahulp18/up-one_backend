29d996828a5c5c5d55e6c1d978fad28d4c0e0e53
rahulp18 <pradhanrahul3346@gmail.com>
2023-10-04 08:56:02 +0530

    cors

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ dotEnv.config();
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 
-app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 
